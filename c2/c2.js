593f5b3f04c9135ac595662326ba6eb8989257dc
Marina <marina.nogovitsyna.98@mail.ru>
2024-06-03 15:58:52 +0300

    Initial commit

---
+++ c2/c2.js
@@ -0,0 +1,3 @@
+document.querySelector('.btn__back').addEventListener('click', () => {
+    window.location.href = "../mainPage/index.html"
+})
---
+++ mainPage/index.js
@@ -0,0 +1,11 @@
+document.querySelector('.choice__images__d7').addEventListener('click', () => {
+    window.location.href = "../d7/d7.html"
+})
+
+document.querySelector('.choice__images__9s').addEventListener('click', () => {
+    window.location.href = "../9s/9s.html"
+})
+
+document.querySelector('.choice__images__c2').addEventListener('click', () => {
+    window.location.href = "../c2/c2.html"
+})
